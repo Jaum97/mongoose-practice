e7deed814cbc9043a13c05894b8c4bc8e5de3a02
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-07-25 22:15:49 -0300

    Create queries

---
+++ queries.js
@@ -0,0 +1,11 @@
+//https://mongoosejs.com/docs/queries.html
+
+var Person = mongoose.model('Person', yourSchema);
+
+// find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
+Person.findOne({ 'name.last': 'Ghost' }, 'name occupation', function (err, person) {
+  if (err) return handleError(err);
+  // Prints "Space Ghost is a talk show host".
+  console.log('%s %s is a %s.', person.name.first, person.name.last,
+    person.occupation);
+});
